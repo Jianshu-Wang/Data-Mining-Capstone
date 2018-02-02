0eb2e98e9db47a9d838bc3b312e95043f8ecc576
JerryTree <wang.jianshu@hotmail.com>
2018-02-02 11:47:25 -0500

    Document name change for mining
    The version get the result of tf-idf similarity from categories

--- tfidfCalculation.py
+++ tfidfCalculation.py
@@ -1,9 +1,41 @@
-d1 = "plot: two teen couples go to a church party, drink and then drive."
-d2 = "films adapted from comic books have had plenty of success , whether they're about superheroes ( batman , superman , spawn ) , or geared toward kids ( casper ) or the arthouse crowd ( ghost world ) , but there's never really been a comic book like from hell before . "
-d3 = "every now and then a movie comes along from a suspect studio , with every indication that it will be a stinker , and to everybody's surprise ( perhaps even the studio ) the film becomes a critical darling . "
-d4 = "damn that y2k bug . "
-documents = [d1, d2, d3, d4]
+# d1 = "plot: two teen couples go to a church party, drink and then drive."
+# d2 = "films adapted from comic books have had plenty of success , whether they're about superheroes ( batman , superman , spawn ) , or geared toward kids ( casper ) or the arthouse crowd ( ghost world ) , but there's never really been a comic book like from hell before . "
+# d3 = "every now and then a movie comes along from a suspect studio , with every indication that it will be a stinker , and to everybody's surprise ( perhaps even the studio ) the film becomes a critical darling . "
+# d4 = "damn that y2k bug . "
+# documents = [d1, d2, d3, d4]
 
+fileName = "Burgers,French,Soul Food,Fondue,Soup,Do It Yourself Food,Ukrainian,Middle Eastern,Coffee & Tea,Wine Bars,Russian,Brazilian,Barbeque,Live Raw Food,Indonesian,Fast Food,Seafood,Ramen,Hawaiian,Latin American,Shanghainese,Juice Bars & Smoothies,Persian Iranian,Kosher,Sandwiches,Tea Rooms,Chicken Wings,Donuts,Cocktail Bars,Desserts"
+fileNameList = fileName.split(",")
+
+documents = []
+path = 'Categories/'
+
+for name in fileNameList:
+    currentPath = path + name + '.txt'
+    with open(currentPath) as f:
+        data = f.read().replace('\n', '')
+        documents.append(data)
+
+
+
+#
+# import glob
+#
+# path = 'Categories/*.txt'
+# files = glob.glob(path)
+#
+# for name in files:
+#     try:
+#         with open(name) as f:
+#             data = f.read().replace('\n', '')
+#             documents.append(data)
+#     except IOError as exc:
+#         if exc.errno != errno.EISDIR:
+#             raise
+
+
+# with open('data.txt', 'r') as myfile:
+#     data=myfile.read().replace('\n', '')
 
 import nltk, string, numpy
 
@@ -69,7 +101,6 @@ print cos_similarity_matrix
 
 import csv
 
-
-with open("result.csv","w+") as my_csv:
+with open("tfidfResult.csv","w+") as my_csv:
     csvWriter = csv.writer(my_csv,delimiter=',')
     csvWriter.writerows(cos_similarity_matrix)
