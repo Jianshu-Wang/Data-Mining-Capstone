288a7db830bccc960bc52f62661573749bb19fad
JerryTree <wang.jianshu@hotmail.com>
2018-02-02 10:32:14 -0500

    Initial Creation of Code file and Result Files

---
+++ tfidfCalculation.py
@@ -0,0 +1,74 @@
+d1 = "plot: two teen couples go to a church party, drink and then drive."
+d2 = "films adapted from comic books have had plenty of success , whether they're about superheroes ( batman , superman , spawn ) , or geared toward kids ( casper ) or the arthouse crowd ( ghost world ) , but there's never really been a comic book like from hell before . "
+d3 = "every now and then a movie comes along from a suspect studio , with every indication that it will be a stinker , and to everybody's surprise ( perhaps even the studio ) the film becomes a critical darling . "
+d4 = "damn that y2k bug . "
+documents = [d1, d2, d3, d4]
+import nltk, string, numpy
+
+nltk.download('punkt')  # first-time use only
+stemmer = nltk.stem.porter.PorterStemmer()
+
+
+def StemTokens(tokens):
+    return [stemmer.stem(token) for token in tokens]
+
+
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+
+def StemNormalize(text):
+    return StemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+nltk.download('wordnet')  # first-time use only
+lemmer = nltk.stem.WordNetLemmatizer()
+
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+LemVectorizer = CountVectorizer(tokenizer=LemNormalize, stop_words='english')
+LemVectorizer.fit_transform(documents)
+print LemVectorizer.vocabulary_
+tf_matrix = LemVectorizer.transform(documents).toarray()
+print tf_matrix
+tf_matrix.shape
+
+from sklearn.feature_extraction.text import TfidfTransformer
+
+tfidfTran = TfidfTransformer(norm="l2")
+tfidfTran.fit(tf_matrix)
+print tfidfTran.idf_
+import math
+def idf(n, df):
+    result = math.log((n + 1.0) / (df + 1.0)) + 1
+    return result
+
+
+print "The idf for terms that appear in one document: " + str(idf(4, 1))
+print "The idf for terms that appear in two documents: " + str(idf(4, 2))
+tfidf_matrix = tfidfTran.transform(tf_matrix)
+print "The tfidf matrix is: "
+print tfidf_matrix.toarray()
+cos_similarity_matrix = (tfidf_matrix * tfidf_matrix.T).toarray()
+print "The cosine similarity matrix is: "
+print cos_similarity_matrix
+
+
+import csv
+
+a = [[1,2,3,4],[5,6,7,8]]
+
+with open("result.csv","w+") as my_csv:
+    csvWriter = csv.writer(my_csv,delimiter=',')
+    csvWriter.writerows(a)
